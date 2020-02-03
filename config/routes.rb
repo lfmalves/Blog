3e4a2ca075ea6911e13e6a3894425037e5b3c855
LFM Alves <luis.projensino@gmail.com>
2020-02-03 11:36:53 -0300

    feat: comments resouces

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :posts
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :posts do 
+  	resources :comments, only: :create
+  end
 end
